05f662285675a30eb07adbf012b2a7d3053e854a
gye200 <Guoliang_Ye@cable.comcast.com>
2019-05-13 17:38:47 -0700

    Update

---
+++ src/go_by_example/worker_pools/worker_pools.go
@@ -0,0 +1,35 @@
+package main
+
+// we’ll look at how to implement a worker pool using goroutines and channels.
+
+import "fmt"
+import "time"
+
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		fmt.Println("worker", id, "started job", j)
+		time.Sleep(time.Second)
+		fmt.Println("worker", id, "finished job", j)
+		results <- j * 2
+	}
+}
+
+func main() {
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+
+	for w := 0; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+
+	for j := 1; j <= 5; j++ {
+		jobs <- j
+	}
+	close(jobs)
+	for a := 1; a <= 5; a++ {
+		<-results
+	}
+	// Our running program shows the 5 jobs being executed by various workers.
+	// The program only takes about 2 seconds despite doing about 5 seconds of total work
+	// because there are 3 workers operating concurrently.
+}
